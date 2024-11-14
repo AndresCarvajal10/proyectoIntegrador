5e662fe1d8096118633e1921e8b0cbb3945a7b65
Luis Carvajal <lfcarvajal342@gmail.com>
2024-11-13 22:03:06 -0500

    Se hace navegación y stack de navegación

--- RNveterinario/App.tsx
+++ RNveterinario/App.tsx
@@ -1,51 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { View, Text, StyleSheet, TextInput, Image} from 'react-native';
+import Navigation from './Navigation';
 
 export default function App() {
   
   return (
-    <View style={styles.container}>
-      <Text style={styles.titulo}>Veterinaria</Text>
-      <Text style={styles.subtitulo}>Regístrate!</Text>
-      <TextInput
-      placeholder='Nombres'
-      style={styles.input}
-      />
-      <TextInput
-      placeholder='Apellidos'
-      style={styles.input}
-      />
-      <StatusBar style="auto" />
-    </View>
+    <Navigation></Navigation>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f1f1f1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  titulo:{
-    fontSize: 40,
-    color: 'black',
-    fontWeight: 'bold'
-  },
-  subtitulo:{
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: 'gray',
-  },
-  input:{
-    borderWidth: 1,
-    color: 'gray',
-    padding: 10,
-    width: '80%',
-    height: 50,
-    marginTop: 20,
-    borderRadius: 20,
-    paddingStart: 20,
-  }
-});
---
+++ RNveterinario/screens/Registro.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export const Registro = () => {
+
+  return (
+      <Text >Login</Text>
+  );
+};
